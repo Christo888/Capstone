d5d773765d9d6ea4a172a6b70219623090ef5867
Christo888 <christojansen1@gmail.com>
2021-06-02 20:18:29 +0200

    Init commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from models import setup_db
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello"
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,109 @@
+import os
+from sqlalchemy import Column, String, create_engine, Integer, Date, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import json
+from sqlalchemy import Column, String, Integer
+
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+
+# database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(6), nullable=False)
+
+    movies = db.relationship('Movie', back_populates='actor')
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        """
+        Create new model object
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """
+        Delete existing model object
+        """
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Update existing model object
+        """
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+        }
+
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(30), nullable=False)
+    release_date = Column(Date)
+
+    actor_id = Column(Integer, ForeignKey('Actor.id'))
+    actor = db.relationship('Actor', back_populates='movie')
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        """
+        Create new model object
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """
+        Delete existing model object
+        """
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Update existing model object
+        """
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'actor_id': self.actor_id,
+            'release_date': self.release_date
+        }

ca02e64c0ddf54a27d09d6d19e05db809f023fa8
Christo888 <christojansen1@gmail.com>
2021-06-07 16:32:12 +0200

    Update auth

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
@@ -6,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
-ALGORITHMS = os.environ['ALGORITHMS']
-API_AUDIENCE = os.environ['API_AUDIENCE']
+AUTH0_DOMAIN = 'nano-degree.eu.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'capstone'
 
 ## AuthError Exception
 '''
